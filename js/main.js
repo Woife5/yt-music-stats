d731ab8d28f55efedb8e7ffece34b842068889c3
Wolfgang Schwendtbauer <wolfgang.schwendtbauer@gmail.com>
2021-12-02 22:55:00 +0100

    added top 25 artists by plays

--- js/main.js
+++ js/main.js
@@ -7,14 +7,7 @@ const reader = new FileReader();
 reader.onload = () => {
     data = JSON.parse(reader.result);
 
-    switch (chartSelector.value) {
-        case 'songs_per_month':
-            displaySongsPerMonth(data);
-            break;
-        default:
-            displaySongsPerMonth(data);
-            break;
-    }
+    updateGraph(data);
 };
 
 // Initialize the data variable with null.
@@ -27,50 +20,58 @@ fileSelector.addEventListener('change', event => {
     reader.readAsText(file);
 });
 
+chartSelector.addEventListener('change', event => {
+    updateGraph(data);
+});
+
+function updateGraph(data) {
+    switch (chartSelector.value) {
+        case 'songs_per_month':
+            displaySongsPerMonth(data);
+            break;
+        case 'top_artists':
+            displayTopArtists(data);
+            break;
+        default:
+            displaySongsPerMonth(data);
+            break;
+    }
+}
+
+/**
+ * Displays the number of times you have listened to a specific artist within the last year.
+ * @param {watch-history.json} data YouTube watch history data
+ */
 function displaySongsPerMonth(data) {
-    // Organize the data to be displayed.
+    let relevantData = getOnlyYTMusicData(data);
 
-    // Get only YT Music data
-    let organizedData = data;
-    organizedData = organizedData.filter(entry => entry.header === 'YouTube Music');
+    let organizedData = {};
+    relevantData.forEach(entry => {
+        let entryData = getRelevantData(entry);
 
-    let relevantData = {};
-    organizedData.forEach(entry => {
-        let key = entry.time.substring(0, 7);
-        if (!relevantData[key]) {
-            relevantData[key] = [];
+        if (entryData) {
+            let key = entryData.time.substring(0, 7);
+
+            if (!organizedData[key]) {
+                organizedData[key] = [];
+            }
+            organizedData[key].push(entryData);
         }
-
-        // Just ignore the entry if the subtitles array is not set,
-        // usually the video/music is no longer available when this happens.
-        if (!entry.subtitles || !entry.subtitles[0] || !entry.subtitles[0].name) {
-            return;
-        }
-
-        let artist = null;
-        if (entry.subtitles[0].name.indexOf(' - Topic') === -1) {
-            artist = entry.subtitles[0].name;
-        } else {
-            artist = entry.subtitles[0].name.substring(0, entry.subtitles[0].name.indexOf(' - Topic'));
-        }
-
-        let entryData = {
-            title: entry.title.substring(8),
-            artist: artist,
-            time: entry.time,
-        };
-
-        relevantData[key].push(entryData);
     });
+
+    if (chart) {
+        chart.destroy();
+        chart = null;
+    }
 
     chart = new Chart(chartCtx, {
         type: 'bar',
         data: {
-            labels: Object.keys(relevantData).reverse(),
+            labels: Object.keys(organizedData).reverse(),
             datasets: [
                 {
                     label: 'Songs played per month',
-                    data: Object.values(relevantData)
+                    data: Object.values(organizedData)
                         .map(element => element.length)
                         .reverse(),
                     backgroundColor: 'rgba(255, 99, 132, 0.2)',
@@ -87,4 +88,92 @@ function displaySongsPerMonth(data) {
             },
         },
     });
+}
+
+/**
+ * Displays the number of songs played per month.
+ * @param {watch-history.json} data YouTube watch history data
+ */
+function displayTopArtists(data) {
+    // Organize the data to be displayed.
+
+    // Get only YT Music data
+    let relevantData = getOnlyYTMusicData(data);
+
+    let organizedData = [];
+    relevantData.forEach(entry => {
+        let entryData = getRelevantData(entry);
+
+        if (entryData) {
+            for (let i = 0; i < organizedData.length; i++) {
+                if (organizedData[i].name === entryData.artist) {
+                    organizedData[i].plays++;
+                    return;
+                }
+            }
+
+            organizedData.push({
+                name: entryData.artist,
+                plays: 1,
+            });
+        }
+    });
+
+    organizedData.sort((a, b) => b.plays - a.plays);
+
+    // Only get the Top 25 artists
+    organizedData = organizedData.slice(0, 25);
+
+    if (chart) {
+        chart.destroy();
+        chart = null;
+    }
+
+    chart = new Chart(chartCtx, {
+        type: 'bar',
+        data: {
+            labels: organizedData.map(element => element.name),
+            datasets: [
+                {
+                    label: 'Plays per artist',
+                    data: organizedData.map(element => element.plays),
+                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                    borderColor: 'rgba(255, 99, 132, 1)',
+                    borderWidth: 1,
+                },
+            ],
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true,
+                },
+            },
+        },
+    });
+}
+
+function getRelevantData(entry) {
+    // Just ignore the entry if the subtitles array is not set,
+    // usually the video/music is no longer available when this happens.
+    if (!entry.subtitles || !entry.subtitles[0] || !entry.subtitles[0].name) {
+        return null;
+    }
+
+    let artist = null;
+    if (entry.subtitles[0].name.indexOf(' - Topic') === -1) {
+        artist = entry.subtitles[0].name;
+    } else {
+        artist = entry.subtitles[0].name.substring(0, entry.subtitles[0].name.indexOf(' - Topic'));
+    }
+
+    return {
+        title: entry.title.substring(8),
+        artist: artist,
+        time: entry.time,
+    };
+}
+
+function getOnlyYTMusicData(data) {
+    return data.filter(entry => entry.header === 'YouTube Music');
 }
