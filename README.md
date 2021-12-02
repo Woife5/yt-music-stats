027863e5e102bb3c99690741dc525bd9ce981b7f
Wolfgang Schwendtbauer <wolfgang.schwendtbauer@gmail.com>
2021-12-02 17:54:43 +0100

    implemented songs by month

--- README.md
+++ README.md
@@ -3,3 +3,30 @@
 This little project visualizes your YouTube Music listening history.
 
 The project is acessible [here](https://woife5.github.io/yt-music-stats/).
+
+Currently this dashboard only supports displaying the amount of songs played per month.
+
+## Usage
+
+Download your data (see [Obtaining your data](#obtaining-your-data)) and then open the [Website](https://woife5.github.io/yt-music-stats/). Click "Choose a file" and select the `watch-history.json` file located in `<path-to>/Takeout/YouTube and YouTube Music/history/watch-history.json`
+
+## Obtaining your data
+
+To get your data, first visit [Google Takeout](https://takeout.google.com). Click the "Deselect all" button at the top and then scroll to the very bottom and only select YouTube data.
+
+![Select only YouTube data](https://i.ibb.co/kgSMbfR/choose-only-youtube.png)
+
+Make sure you configure the following as well by clicking the two buttons directly underneath YouTube:
+
+-   Change history format to `JSON`
+-   Select only the history to be downloaded
+
+### Choose JSON format
+
+Click "Multiple formats" and then select "JSON" next to "history".
+![Choose JSON format](https://i.ibb.co/1GbtW9v/choose-json-format.png)
+
+### Select only history
+
+Click "All YouTube data is included", then "Deselect all" and then only select "history"
+![Select only watch history](https://i.ibb.co/5nkB6dV/choose-only-history.png)
--- js/main.js
+++ js/main.js
@@ -1,17 +1,81 @@
 const fileSelector = document.getElementById('file-selector');
+const chartCtx = document.getElementById('chart').getContext('2d');
 
 // Setup the file reader
 const reader = new FileReader();
 reader.onload = () => {
     data = JSON.parse(reader.result);
-    console.log(data);
+    displaySongsPerMonth(data);
 };
 
 // Initialize the data variable with null.
 let data = null;
+let chart = null;
 
 // Add eventlistener to the file selector to always update the data when a new file is selected.
 fileSelector.addEventListener('change', event => {
     const file = event.target.files[0];
     reader.readAsText(file);
 });
+
+function displaySongsPerMonth(data) {
+    // Organize the data to be displayed.
+
+    // Get only YT Music data
+    let organizedData = data;
+    organizedData = organizedData.filter(entry => entry.header === 'YouTube Music');
+
+    let relevantData = {};
+    organizedData.forEach(entry => {
+        let key = entry.time.substring(0, 7);
+        if (!relevantData[key]) {
+            relevantData[key] = [];
+        }
+
+        // Just ignore the entry if the subtitles array is not set,
+        // usually the video/music is no longer available when this happens.
+        if (!entry.subtitles || !entry.subtitles[0] || !entry.subtitles[0].name) {
+            return;
+        }
+
+        let artist = null;
+        if (entry.subtitles[0].name.indexOf(' - Topic') === -1) {
+            artist = entry.subtitles[0].name;
+        } else {
+            artist = entry.subtitles[0].name.substring(0, entry.subtitles[0].name.indexOf(' - Topic'));
+        }
+
+        let entryData = {
+            title: entry.title.substring(8),
+            artist: artist,
+            time: entry.time,
+        };
+
+        relevantData[key].push(entryData);
+    });
+
+    chart = new Chart(chartCtx, {
+        type: 'bar',
+        data: {
+            labels: Object.keys(relevantData).reverse(),
+            datasets: [
+                {
+                    label: 'Songs played per month',
+                    data: Object.values(relevantData)
+                        .map(element => element.length)
+                        .reverse(),
+                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                    borderColor: 'rgba(255, 99, 132, 1)',
+                    borderWidth: 1,
+                },
+            ],
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true,
+                },
+            },
+        },
+    });
+}
